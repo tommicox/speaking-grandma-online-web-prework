046c7d079d62516a2267962e3ec010021a6f56bb
Tommi Cox <tommicox@gmail.com>
2019-02-28 03:58:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
-speak_to_grandma = "HUH?! SPEAK UP, SONNY!"
-if speak_to_grandma == "i love you too pumpkin!"
-elsif speak_to_grandma == "NO, NOT SINCE 1938!"
-else
-  puts "i love you too pumpkin".upcase
-end
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY"
+  end
